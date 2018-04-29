80c53e2f6ad267b8cc70e3ad9219158ecc04ed09
guilhermesenes <36831112+guilhermesenes@users.noreply.github.com>
2018-04-29 07:38:05 -0300

    Inicio da Codificação do Site

---
+++ ChronoSchool_Codigos/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<link href="style.css" rel="stylesheet"/>
+		<meta charset="UTF-8">
+		<title>Pagina principal</title>
+	</head>
+	<body>
+	
+	<header>
+		<div class="divLogo">
+			<img class="logo" src="logo.png" title="ChronoSchool"/>			
+				<div class="divLogin">
+				<a href="login.php"> 
+					<input type="button" class="botaoHeader" value="LOGIN">
+				</a>
+			</div>
+		</div>
+	</header>
+	
+	<div class="divCorpo">			
+		<div class="divBotaoCorpo">
+		<a href="registro.php"> 
+			<input type="button" class="botaoCorpo	" value="Registre-se">
+		</a>
+		</div>
+	</div>	
+	
+	<footer>
+		<div>
+			<img class="logo" src="logo.png" title="ChronoSchool"/>			
+		</div>
+	</footer>
+	</body>
+</html>
