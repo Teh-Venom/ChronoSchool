2fb8d772d8f2343e08207a8559e0e0fe2d17fb69
Gabriel Braz e Santos <gabrielbrazs@gmail.com>
2018-05-29 19:56:45 -0300

    Verif apache

---
+++ ChronoSchool_Codigos/php/header.php
@@ -0,0 +1,8 @@
+<div class='divLogo'>
+	<img class='logo' src='images/logo.png' title='ChronoSchool'/>			
+		<div class='divLogin'>
+		<a href='login.php'> 
+			<input type='button' class='botaoHeader' value='Login'>
+		</a>
+	</div>
+</div>
---
+++ ChronoSchool_Codigos/php/navbar.php
@@ -0,0 +1,14 @@
+<div id="navbar">
+	<ul>
+		<li><a href="#">Editar Cronogramas</a></li>
+		<li><a href="#">Gerenciar Pessoas</a>
+			<ul>
+				<li><a href="#">Usuários</a></li>
+				<li><a href="#">Professores</a></li>
+			</ul>
+		</li>
+		<li><a href="#">Menu Principal</a></li>
+		<li><a href="#">Ver Cronograma</a></li>
+		<li><a href="#">Gerenciar Instituição</a></li>
+	</ul>
+</div>
