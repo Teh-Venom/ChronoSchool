0fa570e22f6af2574c86e5cb037a5284476967b8
Gabriel Braz e Santos <gabrielbrazs@gmail.com>
2018-06-05 22:30:51 -0300

    hora de ir embora
    ta na hora ta na hora

--- ChronoSchool_Codigos/index.php
+++ ChronoSchool_Codigos/index.php
@@ -6,26 +6,24 @@
 		<title>Pagina principal</title>
 	</head>
 	<body>	
-	<header>
-		<?php
-			include "php/header.php";
-			//include "navbar.php";
-		?>
-		
-	</header>
-	
-	<div class="divCorpo">			
-		<div class="divBotaoCorpo">
-		<a href="registroUsuario.php"> 
-			<input type="button" class="botaoCorpo" value="Registre-se">
-		</a>
-		</div>
-	</div>	
-	
-	<footer>
-		<div>
-			<img class="logo" src="images/logo.png" title="ChronoSchool"/>			
-		</div>
-	</footer>
-	</body>
+
+			<header>
+				<?php
+					include "php/header.php";
+				?>
+			</header>
+			
+				
+			<div class="divBotaoCorpo">
+				<a href="registroUsuario.php"> 
+					<input type="button" class="botaoCorpo" value="Registre-se">
+				</a>
+			</div>
+
+			<footer >
+				<div>
+					<img class="logo" src="images/logo.png" title="ChronoSchool"/>			
+				</div>
+			</footer>
+	</body>	
 </html>
