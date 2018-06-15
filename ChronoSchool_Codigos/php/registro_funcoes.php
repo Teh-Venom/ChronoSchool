1d8092642729d73d76f4f0f9df8110d2ea0b0809
guilhermesenes <36831112+guilhermesenes@users.noreply.github.com>
2018-06-14 21:16:57 -0300

    Changes

--- ChronoSchool_Codigos/php/registro_funcoes.php
+++ ChronoSchool_Codigos/php/registro_funcoes.php
@@ -1,21 +1,4 @@
 <?php
-	function tratamento($nome,$ultimoNome,$email,$senha,$senhaConf)
-	{
-		$nome 		= preg_replace('/[^[:alpha:]_]/', '',$nome);
-		$ultimoNome = preg_replace('/[^[:alpha:]_]/', '',$ultimoNome);
-		$email 		= preg_replace('/[^[:alpha:]_]/', '',$email);
-		$senha 		= preg_replace('/[^[:alpha:]_]/', '',$senha);
-		$senhaConf 	= preg_replace('/[^[:alpha:]_]/', '',$senhaConf);
-		
-		$dados = array(
-			$nome,
-			$ultimoNome,
-			$email,
-			$senha,
-			$senhaConf
-		);
-		return $dados;
-	}
 	function registrar($nome,$ultimoNome,$email,$senha,$senhaConf)
 	{
 		$dados = tratamento($nome,$ultimoNome,$email,$senha,$senhaConf);
