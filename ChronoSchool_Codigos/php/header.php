6c2d23b7969c8bdd3faa19a7567cfc2e36f8a09b
Gabriel Braz e Santos <gabrielbrazs@gmail.com>
2018-06-19 21:22:31 -0300

    bug fixes

--- ChronoSchool_Codigos/php/header.php
+++ ChronoSchool_Codigos/php/header.php
@@ -1,8 +1,21 @@
+<?php
+	$stringBotaoHtml = "<div class='divBotaoLogin'>
+							<a href='login.php'> 
+								<input type='button' class='botaoLoginHeader' value='Login' />
+							</a>
+						</div>";
+	session_start();
+	if(isset($_SESSION)){
+		$stringBotaoHtml = "<div class='divBotaoLogin'>
+								<a href='login.php'> 
+									<input type='button' class='botaoLoginHeader' value='Menu' />
+								</a>
+							</div>";
+	}
+?>
 <div class='divLogoHeader'>
 	<img class='logo' src='images/logo.png' title='ChronoSchool'/>			
 		<div class='divBotaoLogin'>
-			<a href='login.php'> 
-				<input type='button' class='botaoLoginHeader' value='Login'>
-			</a>
+			<?= $stringBotaoHtml?>
 		</div>
 </div>
