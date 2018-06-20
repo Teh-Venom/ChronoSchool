b724c00006667e338d866d471fa37de74133e9f7
guilhermesenes <36831112+guilhermesenes@users.noreply.github.com>
2018-06-19 21:31:56 -0300

    Revert "Merge branch 'master' of https://github.com/gabrielbrazs/ChronoSchool"
    This reverts commit 4bc62191265b268a84a785c8076da159aabf2e7e, reversing
    changes made to 47f654c5fe0f4e29aa0544ac5d2905f83e78b412.

--- ChronoSchool_Codigos/php/header.php
+++ ChronoSchool_Codigos/php/header.php
@@ -1,21 +1,8 @@
-<?php
-	$stringBotaoHtml = "<div class='divBotaoLogin'>
-							<a href='login.php'> 
-								<input type='button' class='botaoLoginHeader' value='Login' />
-							</a>
-						</div>";
-	session_start();
-	if(isset($_SESSION)){
-		$stringBotaoHtml = "<div class='divBotaoLogin'>
-								<a href='login.php'> 
-									<input type='button' class='botaoLoginHeader' value='Menu' />
-								</a>
-							</div>";
-	}
-?>
 <div class='divLogoHeader'>
 	<img class='logo' src='images/logo.png' title='ChronoSchool'/>			
 		<div class='divBotaoLogin'>
-			<?= $stringBotaoHtml?>
+			<a href='login.php'> 
+				<input type='button' class='botaoLoginHeader' value='Login'>
+			</a>
 		</div>
 </div>
